81d4c727fda1cdaa5219c10300a6ea8b011e2ddd
CristiCH2002 <andy76.AFY@gmail.com>
2021-10-20 20:04:11 +0300

    Create main4.py

---
+++ main4.py
@@ -0,0 +1,51 @@
+"Problema 1"
+def numere_intregi():
+    lista = []
+    n = int(input("Numarul de elemente citite este: "))
+    print("Elementele citie sunt:")
+    for i in range(n):
+        ele = int(input())
+        lista.append(ele)
+    return lista
+
+"Problema 2"
+
+def afisare_numar_negativ(lista):
+
+    rez = []
+    for i in lista:
+        
+def test_afisare_numare_negative():
+    assert afisare_numare_negative([-3]) == [-3]
+    assert afisare_numare_negative([]) == []
+    assert afisare_numare_negative([2,3,6,9,-4,-2,0]) == [-4, -2]
+
+
+"Problema 3"
+
+def cel_mai_mic_numar(lista):
+
+    a = int(input("Cifra citita de la tastatura este: ")
+    nr = 0
+    for i in lista:
+        if(i % 10 == a)
+            nr = i
+    for i in lista:
+        if(i % 10 == a)
+            if(i < nr):
+            nr = 1
+    return nr
+
+def test_cel_mai_mic_numar():
+    if(n % i == 0)
+        return False
+    returt True
+
+test_cel_mai_mic_numar
+
+
+
+
+
+
+
